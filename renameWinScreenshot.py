798e71b08b2b6b1edf8808ba0595f008f03a3be7
Sparkle Zheng <16502567+zanjie1999@users.noreply.github.com>
2025-09-28 21:57:24 +0800

    将文件拖到程序上重命名

--- renameWinScreenshot.py
+++ renameWinScreenshot.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 
 # 将Windows的屏幕截图重命名成时间
-# v2 zyyme 20241029
+# v3 zyyme 20241029
 
 import os, datetime, sys
 
@@ -18,24 +18,40 @@ if len(s) == 2:
 startStr = s[0]
 
 listdir = os.listdir('.')
+names = {}
 
+def rename(file_path):
+    # 获取文件的  ctime创建时间 修改时间mtime
+    modified_time = os.path.getmtime(file_path)
+    modified_time_obj = datetime.datetime.fromtimestamp(modified_time)
+    # 格式化修改时间为字符串
+    modified_time_str = modified_time_obj.strftime("%Y-%m-%d_%H-%M-%S")
+    # 获取原始文件名和扩展名
+    basename = os.path.basename(file_path)
+    name, ext = os.path.splitext(basename)
+    # 新的文件名
+    new_name = f"{modified_time_str}{ext}"
+    if new_name in names.keys():
+        names[new_name] += 1
+        new_name = f"{modified_time_str}_{names[new_name]}{ext}"
+    else:
+        names[new_name] = 1
+    # 重命名文件
+    new_path = os.path.join(os.path.dirname(file_path), new_name)
+    print(basename, '->', new_name)
+    os.rename(file_path, new_path)
+
+# 文件拖到
+if len(sys.argv) > 1:
+    # 传入是绝对路径
+    if os.path.basename(sys.argv[1]) != sys.argv[1]:
+        for file_path in sys.argv[1:]:
+            rename(file_path)
+        exit()
+
+# 当前目录
 names = {i:1 for i in listdir}
 for file_path in listdir:
-    if os.path.isfile(file_path) and (not startStr or file_path.startswith(startStr)) and (not endStr or file_path.endswith(endStr)) and file_path != 'renameWinScreenshot.exe' and file_path != 'renameWinScreenshot.py':
-        # 获取文件的  ctime创建时间 修改时间mtime
-        modified_time = os.path.getmtime(file_path)
-        modified_time_obj = datetime.datetime.fromtimestamp(modified_time)
-        # 格式化修改时间为字符串
-        modified_time_str = modified_time_obj.strftime("%Y-%m-%d_%H-%M-%S")
-        # 获取原始文件名和扩展名
-        basename = os.path.basename(file_path)
-        name, ext = os.path.splitext(basename)
-        # 新的文件名
-        new_name = f"{modified_time_str}{ext}"
-        if new_name in names.keys():
-            names[new_name] += 1
-            new_name = f"{modified_time_str}_{names[new_name]}{ext}"
-        # 重命名文件
-        new_path = os.path.join(os.path.dirname(file_path), new_name)
-        print(basename, '->', new_name)
-        os.rename(file_path, new_path)
+    if os.path.isfile(file_path) and ((not startStr or file_path.startswith(startStr)) and (not endStr or file_path.endswith(endStr)) or file_path in sys.argv) and file_path != 'renameWinScreenshot.exe' and file_path != 'renameWinScreenshot.py':
+        rename(file_path)
+
